aa158787287e8bdb0da73e693976ddd525799048
Kamil Lelonek <squixy.sln@gmail.com>
2016-10-09 20:53:51 +0200

    Create Email struct with tests

---
+++ lib/ex_postmark/email.ex
@@ -0,0 +1,394 @@
+defmodule ExPostmark.Email do
+  @moduledoc """
+  Defines an Email.
+
+  This module defines a `ExPostmark.Email` struct and the main functions for composing an email. As it is the contract for
+  the public APIs of ExPostmark it is a good idea to make use of these functions rather than build the struct yourself.
+
+  ## Email fields
+
+  * `from`           - an email address of the sender, example: `{"Tony Stark", "tony.stark@example.com"}`
+  * `to`             - an email address for the recipient(s), example: `[{"Steve Rogers", "steve.rogers@example.com"}]`
+  * `cc`             - an intended carbon copy recipient(s) of the email, example: `[{"Bruce Banner", "hulk.smash@example.com"}]`
+  * `bcc`            - an intended blind carbon copy recipient(s) of the email, example: `[{"Janet Pym", "wasp.avengers@example.com"}]`
+  * `reply_to`       - an email address that should receive replies, example: `{"Clints Barton", "hawk.eye@example.com"}`
+  * `headers`        - a map of headers that should be included in the email, example: `%{"X-Accept-Language" => "en-us, en"}`
+  * `template_id`    - a template to use when sending this message, example: `97854`
+  * `template_model` - a model to be applied to the specified template to generate `HtmlBody`, `TextBody`, and `Subject`, example: `%{team: "Avengers"}`
+
+  ## Examples
+
+      email =
+        new
+        |> to("tony.stark@example.com")
+        |> from("bruce.banner@example.com")
+        |> template_id(97854)
+        |> template_model(%{team: Avengers})
+
+  The composable nature makes it very easy to continue expanding upon a given Email.
+
+      email =
+        email
+        |> cc({"Steve Rogers", "steve.rogers@example.com"})
+        |> cc("wasp.avengers@example.com")
+        |> bcc(["thor.odinson@example.com", {"Henry McCoy", "beast.avengers@example.com"}])
+
+  You can also directly pass arguments to the `new/1` function.
+
+      email = new(from: "tony.stark@example.com", to: "steve.rogers@example.com")
+  """
+
+  import ExPostmark.Formatter
+
+  defstruct from:           nil,
+            to:             [],
+            cc:             [],
+            bcc:            [],
+            reply_to:       nil,
+            headers:        %{},
+            template_id:    nil,
+            template_model: %{}
+
+  @type name    :: String.t
+  @type address :: String.t
+  @type mailbox :: {name, address}
+
+  @type t :: %__MODULE__{
+    from:           mailbox | nil,
+    to:             [mailbox],
+    cc:             [mailbox] | [],
+    bcc:            [mailbox] | [],
+    reply_to:       mailbox | nil,
+    headers:        map(),
+    template_id:    pos_integer(),
+    template_model: map()
+  }
+
+  @doc """
+  Returns a `ExPostmark.Email` struct.
+
+  You can pass a keyword list or a map argument to the function that will be used
+  to populate the fields of that struct. Note that it will silently ignore any
+  fields that it doesn't know about.
+
+  ## Examples
+      iex> new
+      %ExPostmark.Email{}
+
+      iex> new(from: "tony.stark@example.com")
+      %ExPostmark.Email{from: {"", "tony.stark@example.com"}}
+      iex> new(from: {"Tony Stark", "tony.stark@example.com"})
+      %ExPostmark.Email{from: {"Tony Stark", "tony.stark@example.com"}}
+
+      iex> new(to: "steve.rogers@example.com")
+      %ExPostmark.Email{to: [{"", "steve.rogers@example.com"}]}
+      iex> new(to: {"Steve Rogers", "steve.rogers@example.com"})
+      %ExPostmark.Email{to: [{"Steve Rogers", "steve.rogers@example.com"}]}
+      iex> new(to: [{"Bruce Banner", "bruce.banner@example.com"}, "thor.odinson@example.com"])
+      %ExPostmark.Email{to: [{"Bruce Banner", "bruce.banner@example.com"}, {"", "thor.odinson@example.com"}]}
+
+      iex> new(cc: "steve.rogers@example.com")
+      %ExPostmark.Email{cc: [{"", "steve.rogers@example.com"}]}
+      iex> new(cc: {"Steve Rogers", "steve.rogers@example.com"})
+      %ExPostmark.Email{cc: [{"Steve Rogers", "steve.rogers@example.com"}]}
+      iex> new(cc: [{"Bruce Banner", "bruce.banner@example.com"}, "thor.odinson@example.com"])
+      %ExPostmark.Email{cc: [{"Bruce Banner", "bruce.banner@example.com"}, {"", "thor.odinson@example.com"}]}
+
+      iex> new(bcc: "steve.rogers@example.com")
+      %ExPostmark.Email{bcc: [{"", "steve.rogers@example.com"}]}
+      iex> new(bcc: {"Steve Rogers", "steve.rogers@example.com"})
+      %ExPostmark.Email{bcc: [{"Steve Rogers", "steve.rogers@example.com"}]}
+      iex> new(bcc: [{"Bruce Banner", "bruce.banner@example.com"}, "thor.odinson@example.com"])
+      %ExPostmark.Email{bcc: [{"Bruce Banner", "bruce.banner@example.com"}, {"", "thor.odinson@example.com"}]}
+
+      iex> new(reply_to: "edwin.jarvis@example.com")
+      %ExPostmark.Email{reply_to: {"", "edwin.jarvis@example.com"}}
+      iex> new(reply_to: {"Edwin Jarvis", "edwin.jarvis@example.com"})
+      %ExPostmark.Email{reply_to: {"Edwin Jarvis", "edwin.jarvis@example.com"}}
+
+      iex> new(headers: %{"X-Accept-Language" => "en"})
+      %ExPostmark.Email{headers: %{"X-Accept-Language" => "en"}}
+
+      iex> new(template_id: 1)
+      %ExPostmark.Email{template_id: 1}
+
+      iex> new(template_model: %{team: "Avengers"})
+      %ExPostmark.Email{template_model: %{team: "Avengers"}}
+
+  You can obviously combine these arguments together:
+
+      iex> new(to: "steve.rogers@example.com", template_id: 1)
+      %ExPostmark.Email{to: [{"", "steve.rogers@example.com"}], template_id: 1}
+  """
+  @spec new(none | Enum.t) :: t
+  def new(opts \\ []),
+    do: Enum.reduce(opts, %__MODULE__{}, &do_new/2)
+
+  defp do_new({key, value}, email)
+  when key in ~w(from to cc bcc reply_to template_id)a,
+    do: apply(__MODULE__, key, [email, value])
+
+  defp do_new({key, map}, email)
+  when key in ~w(headers template_model)a,
+    do: Enum.reduce(map, email, &put(&1, &2, key))
+
+  defp do_new({key, value}, _email) do
+    raise ArgumentError, message:
+    """
+    invalid field `#{inspect key}` (value=#{inspect value}) for ExPostmark.Email.new/1.
+    """
+  end
+
+  defp put({key, value}, email, name),
+    do: apply(__MODULE__, :"put_#{name}", [email, key, value])
+
+  @doc """
+  Sets a recipient in the `from` field.
+
+  The recipient must be either; a tuple specifying the name and address of the recipient; a string specifying the
+  address of the recipient.
+
+  ## Examples
+
+      iex> new |> from({"Steve Rogers", "steve.rogers@example.com"})
+      %ExPostmark.Email{bcc: [], cc: [],
+       from: {"Steve Rogers", "steve.rogers@example.com"}, headers: %{},
+       reply_to: nil, template_id: nil,
+       template_model: nil, to: []}
+
+      iex> new |> from("steve.rogers@example.com")
+      %ExPostmark.Email{bcc: [], cc: [],
+       from: {"", "steve.rogers@example.com"}, headers: %{},
+       reply_to: nil, template_id: nil, template_model: nil,
+       to: []}
+  """
+  @spec from(t, mailbox | address) :: t
+  def from(%__MODULE__{} = email, from) do
+    from = format_recipient(from)
+    %{email | from: from}
+  end
+
+  @doc """
+  Adds new recipients in the `to` field.
+
+  The recipient must be; a tuple specifying the name and address of the recipient; a string specifying the
+  address of the recipient; or an array comprised of a combination of either.
+
+  ## Examples
+
+      iex> new |> to("steve.rogers@example.com")
+      %ExPostmark.Email{bcc: [], cc: [], from: nil, headers: %{},
+       reply_to: nil, template_id: nil,
+       template_model: nil, to: [{"", "steve.rogers@example.com"}]}
+  """
+  @spec to(t, mailbox | address | [mailbox | address]) :: t
+  def to(%__MODULE__{to: to} = email, recipients) when is_list(recipients) do
+    recipients =
+      recipients
+      |> Enum.map(&format_recipient(&1))
+      |> Enum.concat(to)
+    %{email | to: recipients}
+  end
+  def to(%__MODULE__{} = email, recipient),
+    do: to(email, [recipient])
+
+  @doc """
+  Puts new recipients in the `to` field.
+
+  It will replace any previously added `to` recipients.
+  """
+  @spec put_to(t, mailbox | address | [mailbox | address]) :: t
+  def put_to(%__MODULE__{} = email, recipients) when is_list(recipients) do
+    recipients =
+      recipients
+      |> Enum.map(&format_recipient(&1))
+    %{email | to: recipients}
+  end
+  def put_to(%__MODULE__{} = email, recipient),
+    do: put_to(email, [recipient])
+
+  @doc """
+  Adds new recipients in the `cc` field.
+
+  The recipient must be; a tuple specifying the name and address of the recipient; a string specifying the
+  address of the recipient; or an array comprised of a combination of either.
+
+  ## Examples
+
+      iex> new |> cc("steve.rogers@example.com")
+      %ExPostmark.Email{bcc: [], cc: [{"", "steve.rogers@example.com"}],
+       from: nil, headers: %{}, reply_to: nil,
+       template_id: nil, template_model: nil, to: []}
+  """
+  @spec cc(t, mailbox | address | [mailbox | address]) :: t
+  def cc(%__MODULE__{cc: cc} = email, recipients) when is_list(recipients) do
+    recipients =
+      recipients
+      |> Enum.map(&format_recipient(&1))
+      |> Enum.concat(cc)
+    %{email | cc: recipients}
+  end
+  def cc(%__MODULE__{} = email, recipient),
+    do: cc(email, [recipient])
+
+  @doc """
+  Puts new recipients in the `cc` field.
+
+  It will replace any previously added `cc` recipients.
+  """
+  @spec put_cc(t, mailbox | address | [mailbox | address]) :: t
+  def put_cc(%__MODULE__{} = email, recipients) when is_list(recipients) do
+    recipients =
+      recipients
+      |> Enum.map(&format_recipient(&1))
+    %{email | cc: recipients}
+  end
+  def put_cc(%__MODULE__{} = email, recipient),
+    do: put_cc(email, [recipient])
+
+  @doc """
+  Adds new recipients in the `bcc` field.
+
+  The recipient must be; a tuple specifying the name and address of the recipient; a string specifying the
+  address of the recipient; or an array comprised of a combination of either.
+
+      iex> new |> bcc("steve.rogers@example.com")
+      %ExPostmark.Email{bcc: [{"", "steve.rogers@example.com"}], cc: [],
+       from: nil, headers: %{}, reply_to: nil,
+       template_id: nil, template_model: nil, to: []}
+  """
+  @spec bcc(t, mailbox | address | [mailbox | address]) :: t
+  def bcc(%__MODULE__{bcc: bcc} = email, recipients) when is_list(recipients) do
+    recipients =
+      recipients
+      |> Enum.map(&format_recipient(&1))
+      |> Enum.concat(bcc)
+    %{email | bcc: recipients}
+  end
+  def bcc(%__MODULE__{} = email, recipient),
+    do: bcc(email, [recipient])
+
+  @doc """
+  Puts new recipients in the `bcc` field.
+
+  It will replace any previously added `bcc` recipients.
+  """
+  @spec put_bcc(t, mailbox | address | [mailbox | address]) :: t
+  def put_bcc(%__MODULE__{} = email, recipients) when is_list(recipients) do
+    recipients =
+      recipients
+      |> Enum.map(&format_recipient(&1))
+    %{email | bcc: recipients}
+  end
+  def put_bcc(%__MODULE__{} = email, recipient),
+    do: put_bcc(email, [recipient])
+
+
+  @doc """
+  Sets a recipient in the `reply_to` field.
+
+  The recipient must be either; a tuple specifying the name and address of the recipient; a string specifying the
+  address of the recipient.
+
+  ## Examples
+
+      iex> new |> reply_to({"Steve Rogers", "steve.rogers@example.com"})
+      %ExPostmark.Email{bcc: [], cc: [], from: nil, headers: %{},
+       reply_to: {"Steve Rogers", "steve.rogers@example.com"},
+       template_id: nil, template_model: nil, to: []}
+
+      iex> new |> reply_to("steve.rogers@example.com")
+      %ExPostmark.Email{bcc: [], cc: [], from: nil, headers: %{},
+       reply_to: {"", "steve.rogers@example.com"},
+       template_id: nil, template_model: nil, to: []}
+  """
+  @spec reply_to(t, mailbox | address) :: t
+  def reply_to(%__MODULE__{} = email, reply_to) do
+    reply_to = format_recipient(reply_to)
+    %{email | reply_to: reply_to}
+  end
+
+  @doc """
+  Adds a new `header` in the email.
+
+  The name and value must be specified as strings.
+
+  ## Examples
+
+      iex> new |> put_headers("X-Magic-Number", "7")
+      %ExPostmark.Email{bcc: [], cc: [], from: nil,
+       headers: %{"X-Magic-Number" => "7"}, reply_to: nil,
+       template_id: nil, template_model: nil, to: []}
+  """
+  @spec put_headers(t, String.t, String.t) :: t
+  def put_headers(%__MODULE__{headers: headers} = email, name, value)
+  when is_binary(name) and is_binary(value),
+    do: Map.put(email, :headers, Map.put(headers, name, value))
+  def put_headers(%__MODULE__{}, name, value) do
+    raise ArgumentError, message:
+    """
+    put_headers/3 expects the header name and value to be strings.
+
+    Instead it got:
+      name:  `#{inspect name}`.
+      value: `#{inspect value}`.
+    """
+  end
+
+  @doc """
+  Sets the `template_id` field.
+
+  The template ID must be a positive integer that corresponds to the template ID defined in admin panel.
+
+  ## Examples
+
+      iex> new |> template_id(1)
+      %ExPostmark.Email{bcc: [], cc: [], from: nil, headers: %{},
+       reply_to: nil, template_id: 1,
+       template_model: nil, to: []}
+  """
+  @spec template_id(t, pos_integer()) :: t
+  def template_id(%__MODULE__{} = email, template_id)
+  when is_integer(template_id) and template_id > 0,
+    do: %{email | template_id: template_id}
+  def template_id(%__MODULE__{}, template_id) do
+    raise ArgumentError, message:
+    """
+    template_id/2 expects the template ID to be a positive integer.
+
+    Instead it got:
+      template_id: `#{inspect template_id}`.
+    """
+  end
+
+  @doc """
+  Stores a new **template_model** key and value in the email.
+
+  This store is meant to be for libraries/framework usage. The name should be
+  specified as an atom, the value can be any term.
+
+  ## Examples
+
+      iex> new |> put_template_model(:team, "Avengers")
+      %ExPostmark.Email{bcc: [], cc: [], from: nil, headers: %{},
+       reply_to: nil, template_id: nil,
+       template_model: %{team: "Avengers"}, to: []}
+  """
+  @spec put_template_model(t, atom, any) :: t
+  def put_template_model(%__MODULE__{template_model: nil} = email, key, value)
+  when is_atom(key) and is_binary(value),
+    do: %{email | template_model: %{key => value}}
+  def put_template_model(%__MODULE__{template_model: template_model} = email, key, value)
+  when is_atom(key) and is_binary(value),
+    do: %{email | template_model: Map.put(template_model, key, value)}
+  def put_template_model(%__MODULE__{}, key, value) do
+    raise ArgumentError, message:
+    """
+    put_template_model/3 expects the template model key to be an atom and a value to be a string.
+
+    Instead it got:
+      key:   `#{inspect key}`.
+      value: `#{inspect value}`.
+    """
+  end
+end
---
+++ test/ex_postmark/email_test.exs
@@ -0,0 +1,325 @@
+defmodule ExPostmark.EmailTest do
+  use ExUnit.Case, async: true
+
+  alias ExPostmark.Email
+
+  @name           "Name"
+  @from           "from@example.com"
+  @to             "to@example.com"
+  @cc             "cc@example.com"
+  @bcc            "bcc@example.com"
+  @reply_to       "reply@example.com"
+  @headers        %{"X-Accept-Language" => "pl"}
+  @template_id    1
+  @template_model %{name: "name", team: "team"}
+
+  describe "new/1" do
+    test "should create an empty Email" do
+      assert %Email{} = Email.new()
+    end
+
+    test "should create an Email with the given fields" do
+      email = Email.new(
+        from:           @from,
+        to:             @to,
+        cc:             @cc,
+        bcc:            @bcc,
+        reply_to:       @reply_to,
+        headers:        @headers,
+        template_id:    @template_id,
+        template_model: @template_model
+      )
+
+      assert email.from           == {"", @from}
+      assert email.to             == [{"", @to}]
+      assert email.cc             == [{"", @cc}]
+      assert email.bcc            == [{"", @bcc}]
+      assert email.reply_to       == {"", @reply_to}
+      assert email.headers        == @headers
+      assert email.template_id    == @template_id
+      assert email.template_model == @template_model
+    end
+
+    test "should raise if arguments contain an unknown field" do
+      assert_raise ArgumentError, fn -> Email.new(random: "Random") end
+    end
+  end
+
+  describe "from/2" do
+    test "should put only an email address and an empty name" do
+      email = Email.from(Email.new, @from)
+
+      assert email == %Email{from: {"", @from}}
+    end
+
+    test "should put both an email address and a name" do
+      from  = {@name, @from}
+      email = Email.from(Email.new, from)
+
+      assert email == %Email{from: from}
+    end
+
+    test "should raise if a sender is invalid" do
+      assert_raise ArgumentError, fn -> Email.from(Email.new, nil) end
+      assert_raise ArgumentError, fn -> Email.from(Email.new, "") end
+      assert_raise ArgumentError, fn -> Email.from(Email.new, {nil, nil}) end
+      assert_raise ArgumentError, fn -> Email.from(Email.new, {nil, @from}) end
+      assert_raise ArgumentError, fn -> Email.from(Email.new, {nil, ""}) end
+      assert_raise ArgumentError, fn -> Email.from(Email.new, {"", ""}) end
+    end
+  end
+
+  describe "to/2" do
+    test "should put only an email address and an empty name" do
+      email = Email.to(Email.new, @to)
+
+      assert email == %Email{to: [{"", @to}]}
+    end
+
+    test "should put both an email address and a name" do
+      to    = {@name, @to}
+      email = Email.to(Email.new, to)
+
+      assert email == %Email{to: [to]}
+    end
+
+    test "should put multiple recipients" do
+      to    = {@name, @to}
+      email = Email.to(Email.new, [@to, to])
+
+      assert email == %Email{to: [{"", @to}, to]}
+    end
+
+    test "should raise if a recipients is invalid" do
+      assert_raise ArgumentError, fn -> Email.to(Email.new, nil) end
+      assert_raise ArgumentError, fn -> Email.to(Email.new, "") end
+      assert_raise ArgumentError, fn -> Email.to(Email.new, {nil, nil}) end
+      assert_raise ArgumentError, fn -> Email.to(Email.new, {nil, @to}) end
+      assert_raise ArgumentError, fn -> Email.to(Email.new, {nil, ""}) end
+      assert_raise ArgumentError, fn -> Email.to(Email.new, {"", ""}) end
+    end
+  end
+
+  describe "put_to/2" do
+    test "should replace recipient" do
+      email = Email.new
+      |> Email.to("foo.bar@example.com")
+      |> Email.put_to(@to)
+
+      assert email == %Email{to: [{"", @to}]}
+
+      to    = {@name, @to}
+      email = Email.put_to(email, to)
+
+      assert email == %Email{to: [to]}
+
+      email = Email.put_to(email, [@to, to])
+
+      assert email == %Email{to: [{"", @to}, to]}
+    end
+
+    test "should raise if recipient(s) are invalid" do
+      assert_raise ArgumentError, fn -> Email.put_to(Email.new, nil) end
+      assert_raise ArgumentError, fn -> Email.put_to(Email.new, "") end
+      assert_raise ArgumentError, fn -> Email.put_to(Email.new, {nil, @to}) end
+      assert_raise ArgumentError, fn -> Email.put_to(Email.new, [nil, @to]) end
+      assert_raise ArgumentError, fn ->
+        Email.put_to(Email.new, [{@name, nil}, @to])
+      end
+    end
+  end
+
+  describe "cc/2" do
+    test "should put only an email address and an empty name" do
+      email = Email.cc(Email.new, @cc)
+
+      assert email == %Email{cc: [{"", @cc}]}
+    end
+
+    test "should put both an email address and a name" do
+      cc    = {@name, @cc}
+      email = Email.cc(Email.new, cc)
+
+      assert email == %Email{cc: [cc]}
+    end
+
+    test "should put multiple recipients" do
+      cc    = {@name, @cc}
+      email = Email.cc(Email.new, [@cc, cc])
+
+      assert email == %Email{cc: [{"", @cc}, cc]}
+    end
+
+    test "should raise if a sender is invalid" do
+      assert_raise ArgumentError, fn -> Email.cc(Email.new, nil) end
+      assert_raise ArgumentError, fn -> Email.cc(Email.new, "") end
+      assert_raise ArgumentError, fn -> Email.cc(Email.new, {nil, nil}) end
+      assert_raise ArgumentError, fn -> Email.cc(Email.new, {nil, @cc}) end
+      assert_raise ArgumentError, fn -> Email.cc(Email.new, {nil, ""}) end
+      assert_raise ArgumentError, fn -> Email.cc(Email.new, {"", ""}) end
+    end
+  end
+
+  describe "put_cc/2" do
+    test "should replace recipient" do
+      email = Email.new
+      |> Email.cc("foo.bar@example.com")
+      |> Email.put_cc(@cc)
+
+      assert email == %Email{cc: [{"", @cc}]}
+
+      cc    = {@name, @cc}
+      email = Email.put_cc(email, cc)
+
+      assert email == %Email{cc: [cc]}
+
+      email = Email.put_cc(email, [@cc, cc])
+
+      assert email == %Email{cc: [{"", @cc}, cc]}
+    end
+
+    test "should raise if recipient(s) are invalid" do
+      assert_raise ArgumentError, fn -> Email.put_cc(Email.new, nil) end
+      assert_raise ArgumentError, fn -> Email.put_cc(Email.new, "") end
+      assert_raise ArgumentError, fn -> Email.put_cc(Email.new, {nil, @cc}) end
+      assert_raise ArgumentError, fn -> Email.put_cc(Email.new, [nil, @cc]) end
+      assert_raise ArgumentError, fn ->
+        Email.put_cc(Email.new, [{@name, nil}, @cc])
+      end
+    end
+  end
+
+  describe "bcc/2" do
+    test "should put only an email address and an empty name" do
+      email = Email.bcc(Email.new, @bcc)
+
+      assert email == %Email{bcc: [{"", @bcc}]}
+    end
+
+    test "should put both an email address and a name" do
+      bcc   = {@name, @bcc}
+      email = Email.bcc(Email.new, bcc)
+
+      assert email == %Email{bcc: [bcc]}
+    end
+
+    test "should put multiple recipients" do
+      bcc   = {@name, @bcc}
+      email = Email.bcc(Email.new, [@bcc, bcc])
+
+      assert email == %Email{bcc: [{"", @bcc}, bcc]}
+    end
+
+    test "should raise if a sender is invalid" do
+      assert_raise ArgumentError, fn -> Email.bcc(Email.new, nil) end
+      assert_raise ArgumentError, fn -> Email.bcc(Email.new, "") end
+      assert_raise ArgumentError, fn -> Email.bcc(Email.new, {nil, nil}) end
+      assert_raise ArgumentError, fn -> Email.bcc(Email.new, {nil, @bcc}) end
+      assert_raise ArgumentError, fn -> Email.bcc(Email.new, {nil, ""}) end
+      assert_raise ArgumentError, fn -> Email.bcc(Email.new, {"", ""}) end
+    end
+  end
+
+  describe "put_bcc/2" do
+    test "should replace recipient" do
+      email = Email.new
+      |> Email.bcc("foo.bar@example.com")
+      |> Email.put_bcc(@bcc)
+
+      assert email == %Email{bcc: [{"", @bcc}]}
+
+      bcc   = {@name, @bcc}
+      email = Email.put_bcc(email, bcc)
+
+      assert email == %Email{bcc: [bcc]}
+
+      email = Email.put_bcc(email, [@bcc, bcc])
+
+      assert email == %Email{bcc: [{"", @bcc}, bcc]}
+    end
+
+    test "should raise if recipient(s) are invalid" do
+      assert_raise ArgumentError, fn -> Email.put_bcc(Email.new, nil) end
+      assert_raise ArgumentError, fn -> Email.put_bcc(Email.new, "") end
+      assert_raise ArgumentError, fn -> Email.put_bcc(Email.new, {nil, @bcc}) end
+      assert_raise ArgumentError, fn -> Email.put_bcc(Email.new, [nil, @bcc]) end
+      assert_raise ArgumentError, fn ->
+        Email.put_bcc(Email.new, [{@name, nil}, @bcc])
+      end
+    end
+  end
+
+  describe "reply_to/2" do
+    test "should put only an email address and an empty name" do
+      email = Email.reply_to(Email.new, @reply_to)
+
+      assert email == %Email{reply_to: {"", @reply_to}}
+    end
+
+    test "should put both an email address and a name" do
+      reply_to = {@name, @reply_to}
+      email    = Email.reply_to(Email.new, reply_to)
+
+      assert email == %Email{reply_to: reply_to}
+    end
+
+    test "should raise if a recipient is invalid" do
+      assert_raise ArgumentError, fn -> Email.reply_to(Email.new, nil) end
+      assert_raise ArgumentError, fn -> Email.reply_to(Email.new, "") end
+      assert_raise ArgumentError, fn -> Email.reply_to(Email.new, {nil, nil}) end
+      assert_raise ArgumentError, fn -> Email.reply_to(Email.new, {nil, @reply_to}) end
+      assert_raise ArgumentError, fn -> Email.reply_to(Email.new, {nil, ""}) end
+      assert_raise ArgumentError, fn -> Email.reply_to(Email.new, {"", ""}) end
+    end
+  end
+
+  describe "put_headers/3" do
+    test "should put header" do
+      email = Email.put_headers(Email.new, "X-Accept-Language", "pl")
+
+      assert email == %Email{headers: @headers}
+
+      email = Email.put_headers(email, "X-Mailer", "swoosh")
+
+      assert email == %Email{headers: %{"X-Accept-Language" => "pl", "X-Mailer" => "swoosh"}}
+    end
+
+    test "should raise if a header is invalid" do
+      assert_raise ArgumentError, fn -> Email.put_headers(Email.new, "X-Accept-Language", nil) end
+      assert_raise ArgumentError, fn -> Email.put_headers(Email.new, nil, "en") end
+      assert_raise ArgumentError, fn -> Email.put_headers(Email.new, nil, nil) end
+    end
+  end
+
+  describe "template_id" do
+    test "should put a template ID" do
+      email = Email.template_id(Email.new, @template_id)
+
+      assert email == %Email{template_id: @template_id}
+    end
+
+    test "should raise if a template ID is invalid" do
+      assert_raise ArgumentError, fn -> Email.template_id(Email.new, 0) end
+      assert_raise ArgumentError, fn -> Email.template_id(Email.new, "") end
+      assert_raise ArgumentError, fn -> Email.template_id(Email.new, nil) end
+      assert_raise ArgumentError, fn -> Email.template_id(Email.new, "123") end
+    end
+  end
+
+  describe "put_template_model/3" do
+    test "should put a template model" do
+      email = Email.put_template_model(Email.new, :name, @name)
+
+      assert email == %Email{template_model: %{name: @name}}
+
+      email = Email.put_template_model(email, :team, "Bar")
+
+      assert email == %Email{template_model: %{name: @name, team: "Bar"}}
+    end
+
+    test "should raise if a template model is invalid" do
+      assert_raise ArgumentError, fn -> Email.put_template_model(Email.new, "", nil) end
+      assert_raise ArgumentError, fn -> Email.put_template_model(Email.new, nil, nil) end
+    end
+  end
+end
