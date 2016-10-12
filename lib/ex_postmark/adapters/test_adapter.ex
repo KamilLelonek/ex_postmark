0076cd2d2ae7709f0c5ee5b44e1f38071965c70e
Kamil Lelonek <squixy.sln@gmail.com>
2016-10-12 23:29:26 +0200

    Add Test adapter

---
+++ lib/ex_postmark/adapters/test_adapter.ex
@@ -0,0 +1,22 @@
+defmodule ExPostmark.Adapters.Test do
+  @moduledoc ~S"""
+  An adapter that sends emails as messages to the current process.
+  This is meant to be used during tests.
+
+  ## Example
+      # config/test.exs
+      config :sample, Sample.Mailer,
+        adapter: ExPostmark.Adapters.Test
+
+      # lib/sample/mailer.ex
+      defmodule Sample.Mailer do
+        use ExPostmark.Mailer, otp_app: :sample
+      end
+  """
+  use ExPostmark.Adapter
+
+  def deliver(email, _config) do
+    send(self(), {:email, email})
+    {:ok, %{}}
+  end
+end
