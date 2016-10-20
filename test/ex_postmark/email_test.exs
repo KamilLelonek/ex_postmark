a5da686f7e2e0c2a5376d77f8e562925023fb38e
Kamil Lelonek <squixy.sln@gmail.com>
2016-10-20 22:57:51 +0200

    Update Email tests

--- test/ex_postmark/email_test.exs
+++ test/ex_postmark/email_test.exs
@@ -47,62 +47,62 @@ defmodule ExPostmark.EmailTest do
 
   describe "from/2" do
     test "should put only an email address and an empty name" do
-      email = Email.from(Email.new, @from)
+      email = Email.from(Email.new(), @from)
 
       assert email == %Email{from: {"", @from}}
     end
 
     test "should put both an email address and a name" do
       from  = {@name, @from}
-      email = Email.from(Email.new, from)
+      email = Email.from(Email.new(), from)
 
       assert email == %Email{from: from}
     end
 
     test "should raise if a sender is invalid" do
-      assert_raise ArgumentError, fn -> Email.from(Email.new, nil) end
-      assert_raise ArgumentError, fn -> Email.from(Email.new, "") end
-      assert_raise ArgumentError, fn -> Email.from(Email.new, {nil, nil}) end
-      assert_raise ArgumentError, fn -> Email.from(Email.new, {nil, @from}) end
-      assert_raise ArgumentError, fn -> Email.from(Email.new, {nil, ""}) end
-      assert_raise ArgumentError, fn -> Email.from(Email.new, {"", ""}) end
+      assert_raise ArgumentError, fn -> Email.from(Email.new(), nil) end
+      assert_raise ArgumentError, fn -> Email.from(Email.new(), "") end
+      assert_raise ArgumentError, fn -> Email.from(Email.new(), {nil, nil}) end
+      assert_raise ArgumentError, fn -> Email.from(Email.new(), {nil, @from}) end
+      assert_raise ArgumentError, fn -> Email.from(Email.new(), {nil, ""}) end
+      assert_raise ArgumentError, fn -> Email.from(Email.new(), {"", ""}) end
     end
   end
 
   describe "to/2" do
     test "should put only an email address and an empty name" do
-      email = Email.to(Email.new, @to)
+      email = Email.to(Email.new(), @to)
 
       assert email == %Email{to: [{"", @to}]}
     end
 
     test "should put both an email address and a name" do
       to    = {@name, @to}
-      email = Email.to(Email.new, to)
+      email = Email.to(Email.new(), to)
 
       assert email == %Email{to: [to]}
     end
 
     test "should put multiple recipients" do
       to    = {@name, @to}
-      email = Email.to(Email.new, [@to, to])
+      email = Email.to(Email.new(), [@to, to])
 
       assert email == %Email{to: [{"", @to}, to]}
     end
 
     test "should raise if a recipients is invalid" do
-      assert_raise ArgumentError, fn -> Email.to(Email.new, nil) end
-      assert_raise ArgumentError, fn -> Email.to(Email.new, "") end
-      assert_raise ArgumentError, fn -> Email.to(Email.new, {nil, nil}) end
-      assert_raise ArgumentError, fn -> Email.to(Email.new, {nil, @to}) end
-      assert_raise ArgumentError, fn -> Email.to(Email.new, {nil, ""}) end
-      assert_raise ArgumentError, fn -> Email.to(Email.new, {"", ""}) end
+      assert_raise ArgumentError, fn -> Email.to(Email.new(), nil) end
+      assert_raise ArgumentError, fn -> Email.to(Email.new(), "") end
+      assert_raise ArgumentError, fn -> Email.to(Email.new(), {nil, nil}) end
+      assert_raise ArgumentError, fn -> Email.to(Email.new(), {nil, @to}) end
+      assert_raise ArgumentError, fn -> Email.to(Email.new(), {nil, ""}) end
+      assert_raise ArgumentError, fn -> Email.to(Email.new(), {"", ""}) end
     end
   end
 
   describe "put_to/2" do
     test "should replace recipient" do
-      email = Email.new
+      email = Email.new()
       |> Email.to("foo.bar@example.com")
       |> Email.put_to(@to)
 
@@ -119,50 +119,50 @@ defmodule ExPostmark.EmailTest do
     end
 
     test "should raise if recipient(s) are invalid" do
-      assert_raise ArgumentError, fn -> Email.put_to(Email.new, nil) end
-      assert_raise ArgumentError, fn -> Email.put_to(Email.new, "") end
-      assert_raise ArgumentError, fn -> Email.put_to(Email.new, {nil, @to}) end
-      assert_raise ArgumentError, fn -> Email.put_to(Email.new, [nil, @to]) end
+      assert_raise ArgumentError, fn -> Email.put_to(Email.new(), nil) end
+      assert_raise ArgumentError, fn -> Email.put_to(Email.new(), "") end
+      assert_raise ArgumentError, fn -> Email.put_to(Email.new(), {nil, @to}) end
+      assert_raise ArgumentError, fn -> Email.put_to(Email.new(), [nil, @to]) end
       assert_raise ArgumentError, fn ->
-        Email.put_to(Email.new, [{@name, nil}, @to])
+        Email.put_to(Email.new(), [{@name, nil}, @to])
       end
     end
   end
 
   describe "cc/2" do
     test "should put only an email address and an empty name" do
-      email = Email.cc(Email.new, @cc)
+      email = Email.cc(Email.new(), @cc)
 
       assert email == %Email{cc: [{"", @cc}]}
     end
 
     test "should put both an email address and a name" do
       cc    = {@name, @cc}
-      email = Email.cc(Email.new, cc)
+      email = Email.cc(Email.new(), cc)
 
       assert email == %Email{cc: [cc]}
     end
 
     test "should put multiple recipients" do
       cc    = {@name, @cc}
-      email = Email.cc(Email.new, [@cc, cc])
+      email = Email.cc(Email.new(), [@cc, cc])
 
       assert email == %Email{cc: [{"", @cc}, cc]}
     end
 
     test "should raise if a sender is invalid" do
-      assert_raise ArgumentError, fn -> Email.cc(Email.new, nil) end
-      assert_raise ArgumentError, fn -> Email.cc(Email.new, "") end
-      assert_raise ArgumentError, fn -> Email.cc(Email.new, {nil, nil}) end
-      assert_raise ArgumentError, fn -> Email.cc(Email.new, {nil, @cc}) end
-      assert_raise ArgumentError, fn -> Email.cc(Email.new, {nil, ""}) end
-      assert_raise ArgumentError, fn -> Email.cc(Email.new, {"", ""}) end
+      assert_raise ArgumentError, fn -> Email.cc(Email.new(), nil) end
+      assert_raise ArgumentError, fn -> Email.cc(Email.new(), "") end
+      assert_raise ArgumentError, fn -> Email.cc(Email.new(), {nil, nil}) end
+      assert_raise ArgumentError, fn -> Email.cc(Email.new(), {nil, @cc}) end
+      assert_raise ArgumentError, fn -> Email.cc(Email.new(), {nil, ""}) end
+      assert_raise ArgumentError, fn -> Email.cc(Email.new(), {"", ""}) end
     end
   end
 
   describe "put_cc/2" do
     test "should replace recipient" do
-      email = Email.new
+      email = Email.new()
       |> Email.cc("foo.bar@example.com")
       |> Email.put_cc(@cc)
 
@@ -179,50 +179,50 @@ defmodule ExPostmark.EmailTest do
     end
 
     test "should raise if recipient(s) are invalid" do
-      assert_raise ArgumentError, fn -> Email.put_cc(Email.new, nil) end
-      assert_raise ArgumentError, fn -> Email.put_cc(Email.new, "") end
-      assert_raise ArgumentError, fn -> Email.put_cc(Email.new, {nil, @cc}) end
-      assert_raise ArgumentError, fn -> Email.put_cc(Email.new, [nil, @cc]) end
+      assert_raise ArgumentError, fn -> Email.put_cc(Email.new(), nil) end
+      assert_raise ArgumentError, fn -> Email.put_cc(Email.new(), "") end
+      assert_raise ArgumentError, fn -> Email.put_cc(Email.new(), {nil, @cc}) end
+      assert_raise ArgumentError, fn -> Email.put_cc(Email.new(), [nil, @cc]) end
       assert_raise ArgumentError, fn ->
-        Email.put_cc(Email.new, [{@name, nil}, @cc])
+        Email.put_cc(Email.new(), [{@name, nil}, @cc])
       end
     end
   end
 
   describe "bcc/2" do
     test "should put only an email address and an empty name" do
-      email = Email.bcc(Email.new, @bcc)
+      email = Email.bcc(Email.new(), @bcc)
 
       assert email == %Email{bcc: [{"", @bcc}]}
     end
 
     test "should put both an email address and a name" do
       bcc   = {@name, @bcc}
-      email = Email.bcc(Email.new, bcc)
+      email = Email.bcc(Email.new(), bcc)
 
       assert email == %Email{bcc: [bcc]}
     end
 
     test "should put multiple recipients" do
       bcc   = {@name, @bcc}
-      email = Email.bcc(Email.new, [@bcc, bcc])
+      email = Email.bcc(Email.new(), [@bcc, bcc])
 
       assert email == %Email{bcc: [{"", @bcc}, bcc]}
     end
 
     test "should raise if a sender is invalid" do
-      assert_raise ArgumentError, fn -> Email.bcc(Email.new, nil) end
-      assert_raise ArgumentError, fn -> Email.bcc(Email.new, "") end
-      assert_raise ArgumentError, fn -> Email.bcc(Email.new, {nil, nil}) end
-      assert_raise ArgumentError, fn -> Email.bcc(Email.new, {nil, @bcc}) end
-      assert_raise ArgumentError, fn -> Email.bcc(Email.new, {nil, ""}) end
-      assert_raise ArgumentError, fn -> Email.bcc(Email.new, {"", ""}) end
+      assert_raise ArgumentError, fn -> Email.bcc(Email.new(), nil) end
+      assert_raise ArgumentError, fn -> Email.bcc(Email.new(), "") end
+      assert_raise ArgumentError, fn -> Email.bcc(Email.new(), {nil, nil}) end
+      assert_raise ArgumentError, fn -> Email.bcc(Email.new(), {nil, @bcc}) end
+      assert_raise ArgumentError, fn -> Email.bcc(Email.new(), {nil, ""}) end
+      assert_raise ArgumentError, fn -> Email.bcc(Email.new(), {"", ""}) end
     end
   end
 
   describe "put_bcc/2" do
     test "should replace recipient" do
-      email = Email.new
+      email = Email.new()
       |> Email.bcc("foo.bar@example.com")
       |> Email.put_bcc(@bcc)
 
@@ -239,80 +239,80 @@ defmodule ExPostmark.EmailTest do
     end
 
     test "should raise if recipient(s) are invalid" do
-      assert_raise ArgumentError, fn -> Email.put_bcc(Email.new, nil) end
-      assert_raise ArgumentError, fn -> Email.put_bcc(Email.new, "") end
-      assert_raise ArgumentError, fn -> Email.put_bcc(Email.new, {nil, @bcc}) end
-      assert_raise ArgumentError, fn -> Email.put_bcc(Email.new, [nil, @bcc]) end
+      assert_raise ArgumentError, fn -> Email.put_bcc(Email.new(), nil) end
+      assert_raise ArgumentError, fn -> Email.put_bcc(Email.new(), "") end
+      assert_raise ArgumentError, fn -> Email.put_bcc(Email.new(), {nil, @bcc}) end
+      assert_raise ArgumentError, fn -> Email.put_bcc(Email.new(), [nil, @bcc]) end
       assert_raise ArgumentError, fn ->
-        Email.put_bcc(Email.new, [{@name, nil}, @bcc])
+        Email.put_bcc(Email.new(), [{@name, nil}, @bcc])
       end
     end
   end
 
   describe "reply_to/2" do
     test "should put only an email address and an empty name" do
-      email = Email.reply_to(Email.new, @reply_to)
+      email = Email.reply_to(Email.new(), @reply_to)
 
       assert email == %Email{reply_to: {"", @reply_to}}
     end
 
     test "should put both an email address and a name" do
       reply_to = {@name, @reply_to}
-      email    = Email.reply_to(Email.new, reply_to)
+      email    = Email.reply_to(Email.new(), reply_to)
 
       assert email == %Email{reply_to: reply_to}
     end
 
     test "should raise if a recipient is invalid" do
-      assert_raise ArgumentError, fn -> Email.reply_to(Email.new, nil) end
-      assert_raise ArgumentError, fn -> Email.reply_to(Email.new, "") end
-      assert_raise ArgumentError, fn -> Email.reply_to(Email.new, {nil, nil}) end
-      assert_raise ArgumentError, fn -> Email.reply_to(Email.new, {nil, @reply_to}) end
-      assert_raise ArgumentError, fn -> Email.reply_to(Email.new, {nil, ""}) end
-      assert_raise ArgumentError, fn -> Email.reply_to(Email.new, {"", ""}) end
+      assert_raise ArgumentError, fn -> Email.reply_to(Email.new(), nil) end
+      assert_raise ArgumentError, fn -> Email.reply_to(Email.new(), "") end
+      assert_raise ArgumentError, fn -> Email.reply_to(Email.new(), {nil, nil}) end
+      assert_raise ArgumentError, fn -> Email.reply_to(Email.new(), {nil, @reply_to}) end
+      assert_raise ArgumentError, fn -> Email.reply_to(Email.new(), {nil, ""}) end
+      assert_raise ArgumentError, fn -> Email.reply_to(Email.new(), {"", ""}) end
     end
   end
 
   describe "put_headers/3" do
     test "should put header" do
-      email = Email.put_headers(Email.new, "X-Accept-Language", "pl")
+      email = Email.put_headers(Email.new(), "X-Accept-Language", "pl")
 
       assert email == %Email{headers: @headers}
 
-      email = Email.put_headers(email, "X-Mailer", "swoosh")
+      email = Email.put_headers(email, "X-Mailer", "ex_postmark")
 
-      assert email == %Email{headers: %{"X-Accept-Language" => "pl", "X-Mailer" => "swoosh"}}
+      assert email == %Email{headers: %{"X-Accept-Language" => "pl", "X-Mailer" => "ex_postmark"}}
     end
 
     test "should raise if a header is invalid" do
-      assert_raise ArgumentError, fn -> Email.put_headers(Email.new, "X-Accept-Language", nil) end
-      assert_raise ArgumentError, fn -> Email.put_headers(Email.new, nil, "en") end
-      assert_raise ArgumentError, fn -> Email.put_headers(Email.new, nil, nil) end
+      assert_raise ArgumentError, fn -> Email.put_headers(Email.new(), "X-Accept-Language", nil) end
+      assert_raise ArgumentError, fn -> Email.put_headers(Email.new(), nil, "en") end
+      assert_raise ArgumentError, fn -> Email.put_headers(Email.new(), nil, nil) end
     end
   end
 
   describe "template_id" do
     test "should put a template ID from an integer" do
-      email = Email.template_id(Email.new, @template_id)
+      email = Email.template_id(Email.new(), @template_id)
 
       assert email == %Email{template_id: @template_id}
     end
     test "should put a template ID from a string" do
-      email = Email.template_id(Email.new, to_string(@template_id))
+      email = Email.template_id(Email.new(), to_string(@template_id))
 
       assert email == %Email{template_id: @template_id}
     end
 
     test "should raise if a template ID is invalid" do
-      assert_raise ArgumentError, fn -> Email.template_id(Email.new, 0) end
-      assert_raise ArgumentError, fn -> Email.template_id(Email.new, "") end
-      assert_raise ArgumentError, fn -> Email.template_id(Email.new, nil) end
+      assert_raise ArgumentError, fn -> Email.template_id(Email.new(), 0) end
+      assert_raise ArgumentError, fn -> Email.template_id(Email.new(), "") end
+      assert_raise ArgumentError, fn -> Email.template_id(Email.new(), nil) end
     end
   end
 
   describe "put_template_model/3" do
     test "should put a template model" do
-      email = Email.put_template_model(Email.new, :name, @name)
+      email = Email.put_template_model(Email.new(), :name, @name)
 
       assert email == %Email{template_model: %{name: @name}}
 
@@ -322,8 +322,8 @@ defmodule ExPostmark.EmailTest do
     end
 
     test "should raise if a template model is invalid" do
-      assert_raise ArgumentError, fn -> Email.put_template_model(Email.new, "", nil) end
-      assert_raise ArgumentError, fn -> Email.put_template_model(Email.new, nil, nil) end
+      assert_raise ArgumentError, fn -> Email.put_template_model(Email.new(), "", nil) end
+      assert_raise ArgumentError, fn -> Email.put_template_model(Email.new(), nil, nil) end
     end
   end
 end
